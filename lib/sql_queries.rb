52158f1e47d829a8808b5d3b79b80cd8f694f4cd
Charles Butler <cdbutle@outlook.com>
2020-04-10 14:39:59 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,6 +26,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
    FROM projects
    INNER JOIN pledges
     ON projects.id = pledges.project_id
+    GROUP BY projects.title
    WHERE left_over >= 0"
 end
 
